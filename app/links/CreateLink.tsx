0f9d6c9b347653c5f4f6977f6dfa3857e30f92ba
Alex Brockson <alexbrockson@gmail.com>
2022-12-20 00:45:33 -0500

    changed to supabase

--- app/links/CreateLink.tsx
+++ app/links/CreateLink.tsx
@@ -1,10 +1,17 @@
 'use client';
 
 import React, {useState} from 'react';
-import PocketBase from 'pocketbase';
+// import PocketBase from 'pocketbase';
 import { resolve } from 'node:path/win32';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+// const pb = new PocketBase('http://127.0.0.1:8090');
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://zaeftioepwoangxkukvc.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphZWZ0aW9lcHdvYW5neGt1a3ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE0NDc5NTIsImV4cCI6MTk4NzAyMzk1Mn0.0E6hFfeilDx6eXxPuPSHClvkv6M9rzXT69-5_YGwLxA'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 export default function CreateLink() {
@@ -14,14 +21,15 @@ export default function CreateLink() {
     const create = async(e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log('creating new link');
-        const newLink = {
-            "url": url,
-            "shorturl": shorturl        
-        };
+        // const newLink = {
+        //     "url": url,
+        //     "short_url": shorturl        
+        // };
 
         try {
-            const record = await pb.collection('links').create(newLink);
-            console.log(record);
+            const { data, error } = await supabase
+            .from('Links')
+            .insert({ url: url, short_url: shorturl })            
         }
         catch (err) {
             // if (err == "Failed to create record"){
@@ -32,27 +40,6 @@ export default function CreateLink() {
         finally {
             return false;
         }
-    }
-
-    const createValidated = async() => {
-        console.log('in createValidated');
-
-
-
-        var isValid = await CheckShortURL(shorturl);
-        if (isValid){
-            create;
-        }
-        else {
-            alert("this short url already exists!");
-        }
-
-        // const newLink = {
-        //     "url": url,
-        //     "shorturl": shorturl        
-        // };
-
-        // const record = await pb.collection('links').create(newLink);
     }
 
     return (
@@ -77,29 +64,29 @@ export default function CreateLink() {
     );
 }
 
-async function CheckShortURL(shorty: string): Promise<boolean> {
-    console.log('Checking Short URL');
-    try 
-    {
-        const shortURL = {
-            "shorturl": shorty        
-        };
+// async function CheckShortURL(shorty: string): Promise<boolean> {
+//     console.log('Checking Short URL');
+//     try 
+//     {
+//         const shortURL = {
+//             "shorturl": shorty        
+//         };
 
-        // throw("this is an error!");
-        const record = await pb.collection('links').getFirstListItem(`shorturl="${shorty}"`);
-        if (record) {
-            console.log(record);
-            return false;
-        }
-        else {
-            console.log('looks good');
-            return true;
-        }
-    }
-    catch (err){
-        alert(err);
-        return false;
-    }    
-}
+//         // throw("this is an error!");
+//         const record = await pb.collection('links').getFirstListItem(`shorturl="${shorty}"`);
+//         if (record) {
+//             console.log(record);
+//             return false;
+//         }
+//         else {
+//             console.log('looks good');
+//             return true;
+//         }
+//     }
+//     catch (err){
+//         alert(err);
+//         return false;
+//     }    
+// }
     
     
--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,42 +1,88 @@
+"use client"
+
 import Link from 'next/link'
+// import { createClient } from '@supabase/supabase-js'
+import supabase from '../utils/supabase'
+import { useEffect, useState } from 'react';
+// import { SupabaseClient } from '@supabase/supabase-js'
 
-async function getLinks() {
-    const res = await fetch(
-        'http://127.0.0.1:8090/api/collections/links/records?page=1&perPage=30',
-        { cache: 'no-store'}
-    );
-    const data = await res.json();
-    console.log(data);
-    return data?.items as any[];
+// export const supabaseUrl = process.env.SUPABASE_URL
+// export const supabaseKey = process.env.SUPABASE_KEY
+// // const supabaseUrl = "https://zaeftioepwoangxkukvc.supabase.co"
+// // const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphZWZ0aW9lcHdvYW5neGt1a3ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE0NDc5NTIsImV4cCI6MTk4NzAyMzk1Mn0.0E6hFfeilDx6eXxPuPSHClvkv6M9rzXT69-5_YGwLxA"
+// // const supabase = createClient(supabaseUrl, supabaseKey)
+// export const supabase = createClient(supabaseUrl, supabaseKey)
+
+// type Link = {
+//     id: string
+//     url: string
+//     short_url: string
+//     expiration: Date
+//     created: Date
+// }
+export default function Links() {
+    const [data, setData] = useState<any>(null);
+
+    useEffect(() => { 
+        const getData = async() => {
+            const {data: supabaseData, error} = await supabase.from("Links").select();
+            console.log({data, error});
+            setData(supabaseData);
+        };
+
+        getData();
+    }, []);
+
+    return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
-export default async function LinksPage() {
-    const links = await getLinks();
-    // const links = LINKS;
+// export async function getLinks() {
+//     // const res = await fetch(
+//     //     'http://127.0.0.1:8090/api/collections/links/records?page=1&perPage=30',
+//     //     { cache: 'no-store'}
+//     // );
+//     // const data = await res.json();
+//     const { data } = await supabase
+//         .from('Links')
+//         .select('*');
+//         // console.log(data);
+//     return {
+//         props: {
+//             links: data
+//         }
+        
+//     }
+// }
 
-    return(
-        <div>
-            <h1>Links</h1>
-            <div>
-                {links?.map((link) => {
-                    return <Lnk key={link.id} link={link} />;
-                })}
-            </div>
-        </div>
-    )
-}
+// export default function LinksPage({ links }: {links: Link[] }) {
+//     // const links = await getLinks();
+//     // const links = LINKS;
 
-function Lnk({ link }: any) {
-    const { id, url, shorturl, created, expiration } = link || {};
+//     return(
+//         <div>
+//             <h1>Links</h1>
+//             <div>
+//                 {links?.map((link) => (
+//                     <Lnk key={link.id} link={link} />
+//                 ))}
+//             </div>
+//         </div>
+//     )
+// }
 
-    return (
-        <Link href={`/links/${id}`}>
-            <div>
-                <h2>{shorturl}</h2>
-                <h4>{url}</h4>
-                <p>{created}</p>
-                <p>{expiration}</p>
-            </div>
-        </Link>
-    );
-}
+
+
+// function Lnk({ link }: { link: Link}) {
+//     // const { id, url, short_url, created, expiration } = link || {};
+
+//     return (
+//         <Link href={`/links/${link.id}`}>
+//             <div>
+//                 <h2>{link.short_url}</h2>
+//                 <h4>{link.url}</h4>
+//                 <p>{link.created.toDateString()}</p>
+//                 <p>{link.expiration.toDateString()}</p>
+//             </div>
+//         </Link>
+//     );
+// }
