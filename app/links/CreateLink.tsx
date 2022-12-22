f85f2e075a259704a0924653b3f1b6119d0d84a2
Alex Brockson <alexbrockson@gmail.com>
2022-12-21 23:58:34 -0500

    Alert for return new shorturl to user

--- app/links/CreateLink.tsx
+++ app/links/CreateLink.tsx
@@ -15,6 +15,7 @@ export default function CreateLink() {
     const [errorText, setError] = useState('')
     const [shorturl, setShortURL] = useState('');
     const [url, setURL] = useState('');
+    const [link, setLink] = useState('');
     const [expiration, setValue] = useState<Dayjs | null>(null);
 
 
@@ -53,7 +54,7 @@ export default function CreateLink() {
                             return true;
                         }
                         else {
-                            alert('Your short URL: ' + window.location.href + "/lnk/" + newShortUrl);
+                            setLink('Your short URL: ' + window.location.href + "lnk/" + newShortUrl);
                         }
                     }
                 }
@@ -80,7 +81,7 @@ export default function CreateLink() {
                                 return true;
                             }
                             else {
-                                alert('Your short URL: ' + window.location.href + "/lnk/" + shorturl);
+                                setLink('Your short URL: ' + window.location.href + "lnk/" + shorturl);
                             }
                         }
                         else {
@@ -104,90 +105,101 @@ export default function CreateLink() {
 
 
     return (
-        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm center">
-            <form onSubmit={create}>
-                <div className="form-group mb-6">
-                    <label className="form-label inline-block mb-2 text-gray-700">URL</label>
-                    <input type="text" className="form-control block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
+        <>
+            <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm center">
+                <form onSubmit={create}>
+                    <div className="form-group mb-6">
+                        <label className="form-label inline-block mb-2 text-gray-700">URL</label>
+                        <input type="text" className="form-control block
+                            w-full
+                            px-3
+                            py-1.5
+                            text-base
+                            font-normal
+                            text-gray-700
+                            bg-white bg-clip-padding
+                            border border-solid border-gray-300
+                            rounded
+                            transition
+                            ease-in-out
+                            m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            placeholder="Paste your long URL" onChange={(e) => setURL(e.target.value)} value={url}/>
+                    </div>
+                    <div className="form-group mb-6">
+                        <label className="form-label inline-block mb-2 text-gray-700">Short URL</label>
+                        <input type="text" className="form-control block
+                            w-full
+                            px-3
+                            py-1.5
+                            text-base
+                            font-normal
+                            text-gray-700
+                            bg-white bg-clip-padding
+                            border border-solid border-gray-300
+                            rounded
+                            transition
+                            ease-in-out
+                            m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            placeholder="Custom short URL" onChange={(e) => setShortURL(e.target.value)} value={shorturl}/>
+                        <small className="block mt-1 text-xs text-gray-600">Optional</small>
+                    </div>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <label className="form-label inline-block mb-2 text-gray-700">Expiration</label>
+                        <DatePicker
+                            // label="Expiration Date"
+                            className="form-control block
+                            w-full
+                            px-3
+                            py-1.5
+                            text-base
+                            font-normal
+                            text-gray-700
+                            bg-white bg-clip-padding
+                            rounded
+                            transition
+                            ease-in-out
+                            m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            value={expiration}
+                            onChange={(newValue) => {
+                            setValue(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                        <small className="block mt-1 text-xs text-gray-600">Optional</small>
+                    </LocalizationProvider>
+                    <br/><br/>
+                    <button type="submit" className="
+                        px-6
+                        py-2.5
+                        bg-blue-600
+                        text-white
+                        font-medium
+                        text-xs
+                        leading-tight
+                        uppercase
                         rounded
+                        shadow-md
+                        hover:bg-blue-700 hover:shadow-lg
+                        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                        active:bg-blue-800 active:shadow-lg
                         transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        placeholder="Paste your long URL" onChange={(e) => setURL(e.target.value)} value={url}/>
+                        duration-150
+                        ease-in-out">Submit</button>
+                </form>
+            </div>
+            {link && (
+                <div className="bg-green-100 rounded-lg py-5 px-6 mb-3 text-base text-green-700 inline-flex items-center w-full" role="alert">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path>
+                    </svg>
+                    <strong className="mr-1">Success! </strong> Here's your link: {link}
+                    <button type="button" className="btn-close box-content w-4 h-4 p-1 ml-auto text-green-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-green-900 hover:opacity-75 hover:no-underline" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-                <div className="form-group mb-6">
-                    <label className="form-label inline-block mb-2 text-gray-700">Short URL</label>
-                    <input type="text" className="form-control block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        placeholder="Custom short URL" onChange={(e) => setShortURL(e.target.value)} value={shorturl}/>
-                    <small className="block mt-1 text-xs text-gray-600">Optional</small>
-                </div>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <label className="form-label inline-block mb-2 text-gray-700">Expiration</label>
-                    <DatePicker
-                        // label="Expiration Date"
-                        className="form-control block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        value={expiration}
-                        onChange={(newValue) => {
-                        setValue(newValue);
-                        }}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                    <small className="block mt-1 text-xs text-gray-600">Optional</small>
-                </LocalizationProvider>
-                <br/><br/>
-                <button type="submit" className="
-                    px-6
-                    py-2.5
-                    bg-blue-600
-                    text-white
-                    font-medium
-                    text-xs
-                    leading-tight
-                    uppercase
-                    rounded
-                    shadow-md
-                    hover:bg-blue-700 hover:shadow-lg
-                    focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                    active:bg-blue-800 active:shadow-lg
-                    transition
-                    duration-150
-                    ease-in-out">Submit</button>
-            </form>
-        </div>
+            )}
+        </>
 
     );
 }
